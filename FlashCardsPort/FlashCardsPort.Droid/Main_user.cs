e0b4c2bef33f6a020f10de98e160195e66dc171d
Kinqston <vanek-cs@mail.ru>
2017-06-08 22:23:18 +1000

    create deck user android

--- FlashCardsPort/FlashCardsPort.Droid/Main_user.cs
+++ FlashCardsPort/FlashCardsPort.Droid/Main_user.cs
@@ -16,14 +16,20 @@ namespace FlashCardsPort.Droid
     [Activity(Label = "Main_user")]
     public class Main_user : Activity
     {
+        Button create_deck;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.mian_user);
-
-
+            create_deck = FindViewById<Button>(Resource.Id.decks);
+            create_deck.Click += Create_deck;
             // Create your application here
+        }
+
+        private void Create_deck(object sender, EventArgs e)
+        {
+            StartActivity(typeof(Decks_user));
         }
     }
 }
